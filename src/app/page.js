2e1055221cd1048f633eb552ec62ad2f823b5952
Brentgotte <brent.gotte@gmail.com>
2023-09-27 15:14:50 +0200

    Start project + Movielist component

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+'use client'; // I assume you meant 'use strict'
+import { useState, useEffect } from 'react';
+import Movielist from '@/components/MovieList';
+import './globals.css';
+
+export default function Home() {
+    return(
+        <div>
+            <h1>Home Page</h1>
+            {/* <Movielist /> */}
+        </div>
+    )
+}
---
+++ src/components/MovieList.js
@@ -0,0 +1,35 @@
+'use client'; // I assume you meant 'use strict'
+import { useState, useEffect } from 'react';
+export default function Movielist() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=bdeba0f284b7d753826f7cb651d9cb90&language=en-US&page=1}`);
+            const data = await response.json();
+            setMovies(data.results);
+            setLoading(false);
+        };
+
+        fetchMovies();
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <>  
+            <h1 className='text-3xl font-bold text-center mb-10'>Popular Movies</h1>
+            <div className='grid grid-cols-5 gap-4 '>
+                {movies.map(movie => (
+                    <div key={movie.id}> 
+                        <p className=''>{movie.title}</p>
+                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} width={100} height={100} />
+                    </div>
+                ))}
+            </div>
+        </>
+    );
+}
