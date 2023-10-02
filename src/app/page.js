cfe63f319938592d62ed119b6ca64f575271ee2e
brentgotte <brent.gotte@gmail.com>
2023-10-02 20:32:11 +0200

    Implement carousel + Animation

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 'use client'; 
 import Movielist from '@/components/MovieList/MovieList';
 import './globals.css';
-import MovieCard from '@/components/MovieCard/MovieCard';
+import Moviecarousel from '@/components/MovieCarousel/Moviecarousel';
+import Carouselcall from '@/components/Carouselcall/Carouselcall';
 
 export default function Home() {
     return(
         <div>
-            {/* <h1>Home Page</h1> */}
+            <h1>Home Page</h1>
+
+            <Carouselcall />
             <Movielist />
         </div>
     )
---
+++ src/components/Carouselcall/Carouselcall.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import Moviecarousel from '../MovieCarousel/Moviecarousel';
+
+export default function Carouselcall() {
+    const [movies, setMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=bdeba0f284b7d753826f7cb651d9cb90&language=en-US&page=1`);
+            const data = await response.json();
+            setMovies(data.results);
+            setLoading(false);
+            console.log(data.results);
+        };
+
+        fetchMovies();
+    }, []);
+
+    // Helper function to chunk the movies array
+    const chunkArray = (array, size) => {
+        const chunked = [];
+        let index = 0;
+        while (index < array.length) {
+            chunked.push(array.slice(index, size + index));
+            index += size;
+        }
+        return chunked;
+    }
+
+    const movieChunks = chunkArray(movies, 20); // Change 2 to the number of movies you want per carousel
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }  
+    
+    return (
+        <div className="flex space-x-4">
+            {movieChunks.map((chunk, idx) => (
+                <Moviecarousel key={idx} movies={chunk} />
+            ))}
+        </div>
+    );
+}
