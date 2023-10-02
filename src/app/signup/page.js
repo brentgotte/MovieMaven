e1d1358ec3397d091dd09c0a9e123d37ae1b3a6d
Niek Goedemans <niekgoedemans93@gmail.com>
2023-10-02 21:17:07 +0200

    sign up page changes

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -7,26 +7,36 @@ export default () => {
     <div className="flex justify-center h-screen items-center">
       <div className="bg-white p-12 rounded text-center">
         <div>
-
-        <h1 className="font-semibold text-black">Sign Up!</h1>
-        <div className="py-10">
-          <TextField
-            required
-            id="standard-input"
-            label="Username"
-            variant="standard"
+          <h1 className="font-semibold text-black">Sign Up!</h1>
+          <div className="py-10">
+            <TextField
+              required
+              id="standard-input"
+              label="Username"
+              variant="standard"
             />
-        </div>
-        <div>
-          <TextField
-            required
-            id="standard-password-input"
-            label="Password"
-            type="password"
-            autoComplete="current-password"
-            variant="standard"
+          </div>
+          <div>
+            <TextField
+              required
+              id="standard-password-input"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              variant="standard"
             />
+          </div>
+          <div className="flex flex-col pt-10">
+            <div>
+              <Button className="bg-blue-500 hover:bg-blue-400 text-white font-semibold rounded w-20 h-7 shadow-md">
+                Sign Up
+              </Button>
             </div>
+            <hr className="my-6 h-0.5 border-t-0 bg-black opacity-5"/>
+            <div className="text-black text-xs">
+              Already have an account? <a href="/" className="underline">Login</a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
