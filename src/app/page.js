ab3d7780da2847266faf3a9cddf76866db881b84
Brentgotte <brent.gotte@gmail.com>
2023-10-03 10:57:01 +0200

    implemented footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 'use client'; 
 import Movielist from '@/components/MovieList/MovieList';
 import './globals.css';
-import Moviecarousel from '@/components/MovieCarousel/Moviecarousel';
 import Carouselcall from '@/components/Carouselcall/Carouselcall';
+import Footer from '@/components/Footer/Footer';
 
 export default function Home() {
     return(
@@ -10,6 +10,7 @@ export default function Home() {
                        <h1 className='text-3xl font-bold text-center mb-10'>New And Hot</h1>
             <Carouselcall />
             <Movielist />
+            <Footer />
         </div>
     )
 }
