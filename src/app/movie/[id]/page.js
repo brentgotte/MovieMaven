9a50ce87a33178ebd2ec5905729d16484994f427
Niek Goedemans <niekgoedemans93@gmail.com>
2023-10-10 09:17:47 +0200

    detail page

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,26 +1,10 @@
-import { fetchMovieData } from "@/lib/helper/fetchMovieDetails";
+import { supabase } from 'src/lib/helper/supabase'
 
 export default function Page({ movie }) {
+  console.log(movie.id)
   return (
     <>
-      <div>Movie Title: {movie.title}</div>
-      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-      <p>Overview: {movie.overview}</p>
-      <p>Released: {movie.release_date}</p>
-      <p>Rating: {movie.vote_average}</p>
-      // Add more movie details as needed
+      
     </>
   )
-}
-
-// Fetching movie data for the detail page using getServerSideProps
-export async function getServerSideProps(context) {
-  const movieId = context.params.id;
-  const movieData = await fetchMovieData(movieId); // Fetch the movie data based on movieId using the imported function
-
-  return {
-    props: {
-      movie: movieData
-    }
-  };
 }
