7dcdd65a329e0ad12d8c7020346629994bb845ee
Niek Goedemans <99808100+N-Goedemans@users.noreply.github.com>
2023-10-12 10:41:00 +0200

    g

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,11 +1,11 @@
+'use client'
 import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
-import { supabase } from '@/lib/helper/supabase'
 
 export default function Page({ movie }) {
+
   return (
     <>
-      
+      {movie.title}
     </>
   )
 }
