c2b0750fd1077c3c7cc00131b4b77785ea495ca5
Niek Goedemans <99808100+N-Goedemans@users.noreply.github.com>
2023-10-14 22:28:45 +0200

    detail printed on detail page

--- src/app/movie/[id]/page.js
+++ src/app/movie/[id]/page.js
@@ -1,11 +1,56 @@
 'use client'
-import { useRouter } from 'next/router'
+import { useSearchParams, usePathname } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+import supabase from '@/api/supabaseClient'
+import Image from 'next/image'
+import CardMedia from '@mui/material/CardMedia'
 
-export default function Page({ movie }) {
-
+export default function Page() {
+  const pathName = usePathname()
+  const [movieID, setMovieID] = useState(null);
+  const [movieData, setMovieData] = useState(null);
+  
+  useEffect(() => {
+    const pathParts = pathName.split('/');
+    const id = pathParts[2];
+    
+    if (id) {
+      setMovieID(id);
+    }
+    
+    
+    const getMovieData = async () => {
+      const { data, error } = await supabase.from('movies').select('*').eq('id', id);
+      
+      if (error) {
+        console.error('Error fetching movie data:', error);
+      } else {
+        setMovieData(data);
+      }
+    }
+    
+    getMovieData();
+  }, [pathName])
+  console.log(movieData);
+  
+  
   return (
     <>
-      {movie.title}
+      <div>
+      <CardMedia 
+        image={`https://image.tmdb.org/t/p/w500${movieData?.[0]?.poster_path}`}
+        alt={movieData?.[0]?.title}
+      />
+        <p>
+          Movie: {movieID}
+        </p>
+        <p>
+          Movie title: {movieData?.[0]?.title}
+        </p>
+        <p>
+          Movie overview: {movieData?.[0]?.overview}
+        </p>
+      </div>
     </>
   )
 }
