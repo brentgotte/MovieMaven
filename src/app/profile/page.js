faa7d6492960b5b2fe51d85cefcc5293501d3a44
Diego <163881@student.horizoncollege.nl>
2023-10-18 11:26:03 +0200

    changes

---
+++ src/app/profile/page.js
@@ -0,0 +1,13 @@
+import Profilepage from "@/components/ProfilePage/Profilepage";
+import Footer from "@/components/Footer/Footer";
+
+export const Profile = () => {
+
+    return(
+        <>
+            <Profilepage />
+            <Footer />
+        </>
+    )
+}
+export default Profile;
