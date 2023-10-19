94724bf8b94fdcf2051b1e63a203ef4680cd0b2d
Brentgotte <brent.gotte@gmail.com>
2023-10-19 14:43:58 +0200

    Already claimed text

--- src/app/mylist/parts/ClaimButton.js
+++ src/app/mylist/parts/ClaimButton.js
@@ -1,75 +1,92 @@
-// components/ClaimButton.js
-import { useState, useEffect } from 'react';
-import supabase from '@/api/supabaseClient';
+import { useState, useEffect } from "react";
+import supabase from "@/api/supabaseClient";
+
 const ClaimButton = ({ movieId, session }) => {
   const [claimed, setClaimed] = useState(false);
   const [user, setUser] = useState(null);
-  
+  const [successMessage, setSuccessMessage] = useState(null);
+
   useEffect(() => {
     supabase.auth.getUser().then((res) => {
       setUser(res.data.user);
     });
-    console.log('user: ', user);
     const checkExistingClaim = async () => {
       try {
         const { data: existingClaim, error } = await supabase
-          .from('watchlist')
-          .select('*')
-          .eq('movie_id', movieId)
-          .eq('user_id', user.id);
+          .from("watchlist")
+          .select("*")
+          .eq("movie_id", movieId)
+          .eq("user_id", user.id);
 
         if (error) {
-          console.error('Error checking existing claim:', error.message);
+          console.error("Error checking existing claim:", error.message);
           return;
         }
 
         if (existingClaim && existingClaim.length > 0) {
           setClaimed(true);
-          console.log('claimed Movie: ', existingClaim[0].movie_id);
-          console.log('user:' , existingClaim[0].user_id);
         }
       } catch (error) {
-        console.error('Error checking existing claim:', error.message);
+        console.error("Error checking existing claim:", error.message);
       }
     };
 
     checkExistingClaim();
-  }, [movieId,  user?.id ]);
+  }, [movieId, user?.id]);
 
   const claimMovie = async (has_watched) => {
     try {
       if (!claimed) {
-        console.log('Claiming movie...', movieId, user.id);
-        await supabase
-          .from('watchlist')
-          .insert(
-            [
-              {
-                movie_id: movieId,
-                user_id: user.id, 
-                has_watched: has_watched,
-              },
-            ],
-          );
+        await supabase.from("watchlist").insert([
+          {
+            movie_id: movieId,
+            user_id: user.id,
+            has_watched: has_watched,
+          },
+        ]);
         setClaimed(true);
+        setSuccessMessage("Added successfully!");
       } else {
-        console.log('Movie is already claimed by the user.');
+        setSuccessMessage("Movie is already claimed by the user.");
       }
     } catch (error) {
-      console.error('Error claiming movie:', error.message);
+      console.error("Error claiming movie:", error.message);
     }
   };
 
   return (
-    <>
-    <button onClick={() => {claimMovie(false)}} className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'  >
-      <p>Click here to add to Watchlist!</p>
-    </button>
+    <div className="claim-button-container">
+      <button
+        onClick={() => {
+          claimMovie(false);
+        }}
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        <p>Click here to add to Watchlist!</p>
+      </button>
+      <button
+        onClick={() => {
+          claimMovie(true);
+        }}
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      >
+        <p>Click here to add to Watched!</p>
+      </button>
 
-<button onClick={() => {claimMovie(true)}} className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'  >
-      <p>Click here to add to Watched!</p>
-    </button>
-    </>
+      {successMessage && <p className="success-message">{successMessage}</p>}
+
+      <style jsx>{`
+        .claim-button-container {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+
+        .success-message {
+          margin-top: 10px;
+        }
+      `}</style>
+    </div>
   );
 };
 
