0eee42000713593157c24b544498c781eef432cf
brent.gotte <brent.gotte@gmail.com>
2023-12-13 13:34:28 +0100

    quick commit

--- src/components/profilePicture/profilePicture.js
+++ src/components/profilePicture/profilePicture.js
@@ -1,88 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { MdAccountCircle } from 'react-icons/md';
-import { useProfileData } from '../ProfileData/ProfileData';
-
-export default function ProfilePicture( ) {
-  const { profilePictureUrl, onImageChange } = useProfileData();
-  const [isEditing, setIsEditing] = useState(false);
+import React, { useState } from 'react';
+import supabase from '@/api/supabaseClient';
+const ProfilePictureUpload = () => {
   const [selectedImage, setSelectedImage] = useState(null);
 
-  // Load the saved image URL from localStorage when the component mounts
-  useEffect(() => {
-    const savedImageUrl = localStorage.getItem('profilePicture');
-    if (savedImageUrl) {
-      onImageChange(savedImageUrl);
-    }
-  }, [onImageChange]);
-
-  const handleSaveImage = () => {
-    if (selectedImage) {
-      const reader = new FileReader();
-      reader.onload = (e) => {
-        const dataUrl = e.target.result;
-  
-        // Save the image URL to localStorage
-        localStorage.setItem('profilePicture', dataUrl);
-        console.log('Image saved to localStorage:', dataUrl); // Add this line
-  
-        // Set the image URL in the component state
-        onImageChange(dataUrl);
-        setIsEditing(false);
-      };
-      reader.readAsDataURL(selectedImage);
-    }
-  };
-  
   const handleImageChange = (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      setSelectedImage(file);
-    }
+    setSelectedImage(event.target.files[0]);
   };
 
-  const clearLocalStorage = () => {
-    // Clear the saved image URL from localStorage
-    localStorage.removeItem('profilePicture');
 
-    // Clear the image in the component state
-    onImageChange(null);
+  const uploadImage = async () => {
+    if (!selectedImage) return;
+
+    try {
+      const user = supabase.auth.getUser();
+
+      if (!user) throw new Error('No user logged in');
+
+      const filePath = `ProfilePictures/${user.id}/${selectedImage.name}`;
+      let { error, data } = await supabase.storage
+        .from('ProfilePictures')
+        .upload(filePath, selectedImage);
+
+      if (error) throw error;
+
+      console.log('Upload successful:', data);
+      // Optionally: Update the user's profile with the new image URL
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
   };
 
   return (
-    <>
-      {isEditing ? (
-        <div>
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-            style={{ display: 'none' }}
-            ref={(input) => input && input.click()}
-          />
-          <button className="pr-4" onClick={handleSaveImage}>
-            Save
-          </button>
-          <button onClick={() => setIsEditing(false)}>Cancel</button>
-        </div>
-      ) : (
-        <>
-          {profilePictureUrl ? (
-            <div>
-              <img
-                src={profilePictureUrl}
-                alt="User Avatar"
-                className="rounded-full border-2 border-white"
-                style={{ width: '75px', height: '75px', objectFit: 'cover' }}
-              />
-            </div>
-          ) : (
-            <MdAccountCircle size={100}  />
-          )}
-          
-          <button onClick={() => setIsEditing(true)}>Change Picture</button>
-        
-        </>
-      )}
-    </>
+    <div>
+      <input type="file" accept="image/*" onChange={handleImageChange} />
+      <button onClick={uploadImage}>Upload Image</button>
+    </div>
   );
-}
+};
+
+export default ProfilePictureUpload;
