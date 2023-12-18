71869e5a141c1755566bbe7bab762dcc01cafe44
brent.gotte <brent.gotte@gmail.com>
2023-12-18 10:24:17 +0100

    commit

--- src/components/profilePicture/profilePicture.js
+++ src/components/profilePicture/profilePicture.js
@@ -1,41 +1,64 @@
-import React, { useState } from 'react';
-import supabase from '@/api/supabaseClient';
-const ProfilePictureUpload = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
+  import React, { useState, useEffect } from 'react';
+  import supabase from '@/api/supabaseClient';
 
-  const handleImageChange = (event) => {
-    setSelectedImage(event.target.files[0]);
+  const ProfilePictureUpload = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [imageUrl, setImageUrl] = useState(null); // State to store the image URL
+
+    useEffect(() => {
+      const fetchProfilePicture = async () => {
+        const { data: userData, error } = await supabase.auth.getUser();
+        if (error) {
+          console.error('Error fetching user:', error);
+          return;
+        }
+
+        if (userData.user) {
+          const filePath = `ProfilePictures/${userData.user.id}/${selectedImage}.jpg`;
+          setImageUrl(filePath);
+        }
+      };
+
+      fetchProfilePicture();
+    }, []);
+
+    const handleImageChange = (event) => {
+      setSelectedImage(event.target.files[0]);
+    };
+
+    const uploadImage = async () => {
+      if (!selectedImage) return;
+
+      try {
+        const { data, error: userError } = await supabase.auth.getUser();
+
+        if (userError) throw userError;
+        if (!data.user) throw new Error('No user logged in');
+
+        const user = data.user;
+        const filePath = `ProfilePictures/${user.id}/${selectedImage.name}`;
+
+        const { error: uploadError } = await supabase.storage
+          .from('ProfilePictures')
+          .upload(filePath, selectedImage);
+          setImageUrl(filePath);
+
+        if (uploadError) throw uploadError;
+
+        console.log('Upload successful');
+        console.log('filePath', filePath);
+      } catch (error) {
+        console.error('Error uploading image:', error);
+      }
+    };
+
+    return (
+      <div>
+        <input type="file" accept="image/*" onChange={handleImageChange} />
+        <button onClick={uploadImage}>Upload Image</button>
+        <img src={`https://qwjiorfvdwuxiiezpnio.supabase.co/storage/v1/object/sign/ProfilePictures/${imageUrl}`} alt="Profile" />
+      </div>
+    );
   };
 
-
-  const uploadImage = async () => {
-    if (!selectedImage) return;
-
-    try {
-      const user = supabase.auth.getUser();
-
-      if (!user) throw new Error('No user logged in');
-
-      const filePath = `ProfilePictures/${user.id}/${selectedImage.name}`;
-      let { error, data } = await supabase.storage
-        .from('ProfilePictures')
-        .upload(filePath, selectedImage);
-
-      if (error) throw error;
-
-      console.log('Upload successful:', data);
-      // Optionally: Update the user's profile with the new image URL
-    } catch (error) {
-      console.error('Error uploading image:', error);
-    }
-  };
-
-  return (
-    <div>
-      <input type="file" accept="image/*" onChange={handleImageChange} />
-      <button onClick={uploadImage}>Upload Image</button>
-    </div>
-  );
-};
-
-export default ProfilePictureUpload;
+  export default ProfilePictureUpload;
