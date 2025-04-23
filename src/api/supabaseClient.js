71d569cd65553121316f8c6bd635492234a0faa6
brentgotte <brent.gotte@gmail.com>
2025-04-24 01:50:00 +0200

    quick commit

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qwjiorfvdwuxiiezpnio.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3amlvcmZ2ZHd1eGlpZXpwbmlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY0ODk2MzcsImV4cCI6MjAxMjA2NTYzN30.7DY0ZLaOVS1FmhXWe17Gv_mFwfuBc-LhtpXuHimdntg'
+const supabaseUrl = 'http://127.0.0.1:54321'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
